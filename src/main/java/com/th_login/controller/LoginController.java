d318f4ca39e8f50a9c6d6d00d6bb0b3db5d507ea
truongphan4898 <truongphan4898@gmail.com>
2024-05-29 11:13:41 +0700

    add

---
+++ src/main/java/com/th_login/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.th_login.controller;
+
+
+
+
+import com.th_login.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public ModelAndView loginPage() {
+        ModelAndView modelAndView = new ModelAndView("/login");
+        modelAndView.addObject("user", new User());
+        return modelAndView;
+    }
+
+    @PostMapping("/doLogin")
+    public ModelAndView login(@ModelAttribute User user) {
+        ModelAndView modelAndView = new ModelAndView("/dashboard");
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+}
